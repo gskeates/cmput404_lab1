8f60f352a2e1c4742c9f09cf4d2cf3ed59c5f028
gskeates <gskeates@ualberta.ca>
2021-01-13 20:40:17 -0700

    Fixes error

--- lab1.py
+++ lab1.py
@@ -1,8 +1,8 @@
 import requests
 
-# print(requests.__version__)
+print(requests.__version__)
 
 r1 = requests.get('http://google.com')
 
-r2 = requests.get('https://raw.githubusercontent.com/gskeates/cmput404_lab1/master/lab1.py')
+r2 = requests.get('http://raw.githubusercontent.com/gskeates/cmput404_lab1/master/lab1.py')
 print(r2.text)
